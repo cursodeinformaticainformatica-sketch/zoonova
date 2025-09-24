df5f485994bd7d6769efa274307d555135d71751
alex <cursodeinformaticainformatica@gmail.com>
2025-09-24 10:59:26 -0600

    Delete js directory

--- js/app5.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const respuesta = document.getElementById("respuesta");
-  respuesta.classList.remove("oculto");
-  this.reset();
-});
